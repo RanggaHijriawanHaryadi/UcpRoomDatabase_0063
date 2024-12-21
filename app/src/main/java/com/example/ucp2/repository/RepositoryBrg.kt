9d978d45be61523345cf5cdbccef48eebbb39746
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-12-21 16:02:23 +0700

    Membuat RepositoryBrg untuk operasi CRUD pada barang

---
+++ app/src/main/java/com/example/ucp2/repository/RepositoryBrg.kt
@@ -0,0 +1,13 @@
+package com.example.ucp2.repository
+
+import com.example.ucp2.data.entity.Barang
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryBrg {
+
+    suspend fun insertBarang(barang: Barang)
+    fun getAllBarang(): Flow<List<Barang>>
+    fun getBarang(id: String) : Flow<Barang>
+    suspend fun updateBarang(barang: Barang)
+    suspend fun deleteBarang(barang: Barang)
+}
