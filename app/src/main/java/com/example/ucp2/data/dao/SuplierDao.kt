034844ea1577369137caa050f836e3fa0b59ce11
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-12-21 15:29:54 +0700

    Menambahkan sistem operasi Create, Read pada suplier

---
+++ app/src/main/java/com/example/ucp2/data/dao/SuplierDao.kt
@@ -0,0 +1,21 @@
+package com.example.ucp2.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.ucp2.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SuplierDao {
+    @Insert
+    suspend fun insertSuplier(
+        suplier: Suplier
+    )
+
+    @Query("SELECT * FROM suplier ORDER BY nama ASC")
+    fun getAllSuplier() : Flow<List<Suplier>>
+
+    @Query("SELECT * FROM SUPLIER WHERE id = :id")
+    fun getSuplier(id: String): Flow<Suplier>
+}
