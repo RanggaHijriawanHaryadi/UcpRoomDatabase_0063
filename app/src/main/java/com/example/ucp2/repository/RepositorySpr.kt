8c350ff797f9cb9eace27281ece747cea7720fc9
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-12-21 15:51:30 +0700

    Membuat RepositorySpr untuk operasi Create, Read

---
+++ app/src/main/java/com/example/ucp2/repository/RepositorySpr.kt
@@ -0,0 +1,12 @@
+package com.example.ucp2.repository
+
+import com.example.ucp2.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
+
+interface RepositorySpr {
+
+    suspend fun insertSuplier(suplier: Suplier)
+    fun getAllSuplier(): Flow<List<Suplier>>
+    fun getSuplier(id: String) : Flow<Suplier>
+
+}
