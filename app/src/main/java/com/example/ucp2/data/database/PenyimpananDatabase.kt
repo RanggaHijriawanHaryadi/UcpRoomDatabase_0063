738ca66b68a8578e8587494a01b67a76a8811f3c
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-12-22 23:09:06 +0700

    Membuat 2 tabel untuk Barang dan Suplier pada database

---
+++ app/src/main/java/com/example/ucp2/data/database/PenyimpananDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.ucp2.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.ucp2.data.dao.BarangDao
+import com.example.ucp2.data.dao.SuplierDao
+import com.example.ucp2.data.entity.Barang
+import com.example.ucp2.data.entity.Suplier
+
+@Database(entities = [Barang::class, Suplier::class], version = 2, exportSchema = false)
+abstract class PenyimpananDatabase : RoomDatabase(){
+
+    abstract fun barangDao(): BarangDao
+    abstract fun suplierDao(): SuplierDao
+
+    companion object{
+        @Volatile
+        private var Instance: PenyimpananDatabase? = null
+
+        fun getDatabase(context: Context): PenyimpananDatabase {
+            return (Instance ?: synchronized(this){
+                Room.databaseBuilder(
+                    context,
+                    PenyimpananDatabase::class.java,
+                    "PenyimpananDatabase2"
+                )
+                    .build().also { Instance = it }
+            })
+        }
+    }
+}
