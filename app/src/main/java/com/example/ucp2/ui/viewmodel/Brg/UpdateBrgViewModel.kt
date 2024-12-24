0c77cef3458562e4d4305d6a91e97ac0e8f15661
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-12-24 08:05:00 +0700

    Menambah UpdateState, validasi dataBrg dengan getBarang pada UpdateBrgViewModel.kt

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/Brg/UpdateBrgViewModel.kt
@@ -0,0 +1,87 @@
+package com.example.ucp2.ui.viewmodel.Brg
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Barang
+import com.example.ucp2.repository.RepositoryBrg
+import com.example.ucp2.ui.navigation.DestinasiUpdateBrg
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class UpdateBrgViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryBrg: RepositoryBrg
+): ViewModel(){
+
+    var updateUIState by mutableStateOf(BrgUIState())
+        private set
+
+    private val _id: String = checkNotNull(savedStateHandle[DestinasiUpdateBrg.ID])
+
+    init {
+        viewModelScope.launch {
+            updateUIState = repositoryBrg.getBarang(_id)
+                .filterNotNull()
+                .first()
+                .toUiStateBrg()
+        }
+    }
+
+    fun updateState (barangEvent: BarangEvent) {
+        updateUIState = updateUIState.copy(
+            barangEvent = barangEvent,
+        )
+    }
+    fun validateFields() : Boolean{
+        val event = updateUIState.barangEvent
+        val errorState = FormErrorStateBrg(
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            deskripsi = if (event.deskripsi.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+            harga =  if (event.harga.isNotEmpty()) null else "Alamat tidak boleh kosong",
+            stok = if (event.stok.isNotEmpty()) null else "Kelas tidak boleh kosong",
+            namaSuplier = if (event.namaSuplier.isNotEmpty()) null else "Angkatan tidak boleh kosong"
+        )
+
+        updateUIState = updateUIState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun updateData() {
+        val currentEvent = updateUIState.barangEvent
+
+        if (validateFields()) {
+            viewModelScope.launch {
+                try {
+                    repositoryBrg.updateBarang(currentEvent.toBarangEntity())
+                    updateUIState = updateUIState.copy(
+                        snackBarMessage = "Data berhasil diupdate",
+                        barangEvent = BarangEvent(),
+                        isEntryValid = FormErrorStateBrg()
+                    )
+                    println("snackBarMessage diatur: ${updateUIState.
+                    snackBarMessage}")
+                } catch (e: Exception) {
+                    updateUIState = updateUIState.copy(
+                        snackBarMessage = "Data gagal diupdate"
+                    )
+                }
+            }
+        }else{
+            updateUIState = updateUIState.copy(
+                snackBarMessage = "Data gagal diupdate"
+            )
+        }
+    }
+    fun resetSnackBarMessage(){
+        updateUIState = updateUIState.copy(snackBarMessage = null)
+    }
+}
+
+fun Barang.toUiStateBrg() : BrgUIState = BrgUIState(
+    barangEvent = this.toDetailUiEvent(),
+)
